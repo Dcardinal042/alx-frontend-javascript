a0474a3b440a798b1cd15660c0bf33578b2a5225
Dcardinal042 <samokoh40@gmail.com>
2023-11-30 00:02:55 +0000

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-// 0-promise.js
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Your asynchronous code here
-    resolve("Successfully resolved!");
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
-
